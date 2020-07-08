40547059658118cbba90061eddea3ebdb65359af
Chung Wei <leongchungwei@hotmail.com>
2020-07-08 22:03:37 +0800

    Bundled fonts assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_flutter/models/TodoModel.dart';
-
 import 'package:todo_flutter/screens/HomeScreen.dart';
 import 'package:todo_flutter/var/string.dart';
 import 'package:todo_flutter/var/theme.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(
     ChangeNotifierProvider(
       create: (_) => TodoModel(),
--- lib/var/theme.dart
+++ lib/var/theme.dart
@@ -4,39 +4,45 @@ import 'package:google_fonts/google_fonts.dart';
 const double _radius = 14;
 final BorderRadius _borderRadius = BorderRadius.circular(_radius);
 
-ThemeData getTheme(BuildContext context) => ThemeData(
-      primarySwatch: Colors.deepPurple,
-      visualDensity: VisualDensity.adaptivePlatformDensity,
-      textTheme: GoogleFonts.rubikTextTheme(Theme.of(context).textTheme),
-      fontFamily: GoogleFonts.rubik().fontFamily,
-      snackBarTheme: SnackBarThemeData(
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: _borderRadius,
+ThemeData getTheme(BuildContext context) {
+  final TextTheme textTheme =
+      GoogleFonts.ralewayTextTheme(Theme.of(context).textTheme);
+  final String fontFamily = GoogleFonts.raleway().fontFamily;
+
+  return ThemeData(
+    primarySwatch: Colors.deepPurple,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    textTheme: textTheme,
+    fontFamily: fontFamily,
+    snackBarTheme: SnackBarThemeData(
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: _borderRadius,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: _borderRadius,
+      ),
+    ),
+    buttonTheme: ButtonThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: _borderRadius,
+      ),
+    ),
+    bottomSheetTheme: BottomSheetThemeData(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(_radius),
+          topRight: Radius.circular(_radius),
         ),
       ),
-      inputDecorationTheme: InputDecorationTheme(
-        border: OutlineInputBorder(
-          borderRadius: _borderRadius,
-        ),
-      ),
-      buttonTheme: ButtonThemeData(
-        shape: RoundedRectangleBorder(
-          borderRadius: _borderRadius,
-        ),
-      ),
-      bottomSheetTheme: BottomSheetThemeData(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(_radius),
-            topRight: Radius.circular(_radius),
-          ),
-        ),
-      ),
-      appBarTheme: AppBarTheme(
-        color: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black),
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: Colors.black),
-      ),
-    );
+    ),
+    appBarTheme: AppBarTheme(
+      color: Colors.transparent,
+      elevation: 0,
+      iconTheme: IconThemeData(color: Colors.black),
+      textTheme: textTheme.apply(bodyColor: Colors.black),
+    ),
+  );
+}
